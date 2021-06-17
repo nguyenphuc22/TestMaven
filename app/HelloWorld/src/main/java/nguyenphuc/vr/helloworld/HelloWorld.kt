7d5f52105ad6e9c51dc3065216b3325966c57241
nguyenphuc22 <nguyenphuc220100@Gmail.com>
2021-06-17 11:03:19 +0700

    Add module hello world
    Add module hello world

---
+++ app/HelloWorld/src/main/java/nguyenphuc/vr/helloworld/HelloWorld.kt
@@ -0,0 +1,7 @@
+package nguyenphuc.vr.helloworld
+
+class HelloWorld {
+    public fun printlnHellWorld() {
+        print("Hello World")
+    }
+}
--- app/src/main/java/nguyenphuc/vr/testmaven/MainActivity.kt
+++ app/src/main/java/nguyenphuc/vr/testmaven/MainActivity.kt
@@ -2,10 +2,13 @@ package nguyenphuc.vr.testmaven
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import nguyenphuc.vr.helloworld.HelloWorld
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var helloWorld: HelloWorld = HelloWorld()
+        helloWorld.printlnHellWorld()
     }
 }
